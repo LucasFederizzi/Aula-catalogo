d76a1cf6ee0f9c18f12743c77d0a438c4b6a6eeb
Lucas Federizzi <Federizzi27@gmail.com>
2025-06-07 00:20:48 +0000

    Add, Header, ProductCatalog

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ProductCatalog  } from "./Components/ProductCatalog"
 
 function App() {
   return (
-  <div className="bg-gray-100 min-h-screen">Add commentMore actions
+  <div className="bg-gray-100 min-h-screen">
       <div className="max-w-3xl mx-auto">
         <Header />
         <ProductCatalog />
